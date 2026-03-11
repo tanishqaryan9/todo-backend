7448e227e5fe8d16c6159931a20b510cd8284ef1
Tanishq Aryan <aryantanishq123@gmail.com>
2026-03-11 14:35:26 +0530

    Keep backend active

--- src/main/java/com/todoApp/backend/config/SecurityConfig.java
+++ src/main/java/com/todoApp/backend/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+			.requestMatchers("/actuator/health").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
